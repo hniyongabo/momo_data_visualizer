3c1c8fced50e002528fed238fd4adaed8dd53cb3
kethia19 <k.kayigire@alustudent.com>
2025-09-19 10:50:21 +0200

    Adding the database file

---
+++ database/database_setup.sql
@@ -0,0 +1,196 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+--
+-- Host: localhost    Database: momo_visualizer
+-- ------------------------------------------------------
+-- Server version	8.0.41
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+  `category_id` int NOT NULL AUTO_INCREMENT,
+  `transaction_type` enum('DEBIT','CREDIT') NOT NULL,
+  `payment_type` enum('CASH','MoMoPay','Airtime') NOT NULL,
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `uq_category` (`transaction_type`,`payment_type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `message`
+--
+
+DROP TABLE IF EXISTS `message`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `message` (
+  `message_id` int NOT NULL AUTO_INCREMENT,
+  `transaction_id` int NOT NULL,
+  `is_read` tinyint(1) DEFAULT '0',
+  `subject` varchar(100) DEFAULT NULL,
+  `body` varchar(800) DEFAULT NULL,
+  `sms_protocol` varchar(20) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `contact_name` varchar(100) DEFAULT NULL,
+  `sub_id` int DEFAULT NULL,
+  PRIMARY KEY (`message_id`),
+  UNIQUE KEY `transaction_id` (`transaction_id`),
+  CONSTRAINT `fk_message_transaction` FOREIGN KEY (`transaction_id`) REFERENCES `transaction_record` (`transaction_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `message`
+--
+
+LOCK TABLES `message` WRITE;
+/*!40000 ALTER TABLE `message` DISABLE KEYS */;
+/*!40000 ALTER TABLE `message` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `system_logs`
+--
+
+DROP TABLE IF EXISTS `system_logs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `system_logs` (
+  `log_id` int NOT NULL AUTO_INCREMENT,
+  `log_text` text NOT NULL,
+  `log_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`log_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `system_logs`
+--
+
+LOCK TABLES `system_logs` WRITE;
+/*!40000 ALTER TABLE `system_logs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `system_logs` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction_category`
+--
+
+DROP TABLE IF EXISTS `transaction_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transaction_category` (
+  `transaction_id` int NOT NULL,
+  `category_id` int NOT NULL,
+  PRIMARY KEY (`transaction_id`,`category_id`),
+  KEY `fk_tc_category` (`category_id`),
+  CONSTRAINT `fk_tc_category` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_tc_transaction` FOREIGN KEY (`transaction_id`) REFERENCES `transaction_record` (`transaction_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction_category`
+--
+
+LOCK TABLES `transaction_category` WRITE;
+/*!40000 ALTER TABLE `transaction_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transaction_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `transaction_record`
+--
+
+DROP TABLE IF EXISTS `transaction_record`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `transaction_record` (
+  `transaction_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `transaction_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `toa` varchar(50) DEFAULT NULL COMMENT 'Type of account',
+  `sc_toa` varchar(50) DEFAULT NULL COMMENT 'Service center to account info',
+  `readable_date` date DEFAULT NULL,
+  `amount` decimal(12,2) NOT NULL,
+  `status` enum('COMPLETED','FAILED','PENDING') NOT NULL,
+  `service_center_number` varchar(20) DEFAULT NULL,
+  `sender_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`transaction_id`),
+  KEY `idx_user` (`user_id`),
+  KEY `idx_status` (`status`),
+  CONSTRAINT `fk_transaction_user` FOREIGN KEY (`user_id`) REFERENCES `user_account` (`user_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `transaction_record`
+--
+
+LOCK TABLES `transaction_record` WRITE;
+/*!40000 ALTER TABLE `transaction_record` DISABLE KEYS */;
+/*!40000 ALTER TABLE `transaction_record` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_account`
+--
+
+DROP TABLE IF EXISTS `user_account`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_account` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `phone_number` varchar(20) NOT NULL,
+  `old_balance` decimal(12,2) DEFAULT '0.00',
+  `current_balance` decimal(12,2) NOT NULL DEFAULT '0.00',
+  `user_type` enum('personal','merchant','agent') NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `phone_number` (`phone_number`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_account`
+--
+
+LOCK TABLES `user_account` WRITE;
+/*!40000 ALTER TABLE `user_account` DISABLE KEYS */;
+INSERT INTO `user_account` VALUES (1,'0780000001',12000.00,11000.00,'personal');
+/*!40000 ALTER TABLE `user_account` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-09-19 10:45:03
