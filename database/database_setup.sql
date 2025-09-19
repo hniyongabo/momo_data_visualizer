3f93c41557e29573f09937f75722130a922297aa
Habeeb Dindi <h.niyongabo@alustudent.com>
2025-09-19 22:13:31 +0200

    Add create database statements

--- database/database_setup.sql
+++ database/database_setup.sql
@@ -19,6 +19,10 @@
 -- Table structure for table `category`
 --
 
+CREATE DATABASE IF NOT EXISTS momo_data_visualizer;
+
+USE momo_data_visualizer;
+
 DROP TABLE IF EXISTS `category`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
